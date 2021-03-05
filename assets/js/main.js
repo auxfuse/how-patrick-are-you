651b37b749344e370cca1cf5962888cbaaadaee8
Anthony O' Brien <aux.fuse@yahoo.com>
2021-03-05 23:35:03 +0000

    Default navbar added, with accompanying JS to toggle mobile menu. Also, root variables and default css structure defined

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 // import libraries, packages etc...
 import startFirebase from "./firebase.js";
 
+// Navbar toggler implementation
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerIcon = document.querySelector('#burger-icon');
+    const navLinks = document.querySelector('#nav-links');
+
+    // Add "is-active" class to navLinks & burgerIcon to change default Bulma utility display states on click of burger-icon
+    burgerIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('is-active');
+        burgerIcon.classList.toggle('is-active');
+    });
+});
+
 // initiliazes the firebase app (firebase.js) so we can use the functions to access data
 startFirebase();

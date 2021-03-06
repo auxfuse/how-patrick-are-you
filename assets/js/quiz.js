6d057fbf1685bf79e764d7a72d10db5a43766c59
Kenny Wilde <kendevwilde@gmail.com>
2021-03-06 21:07:32 +0000

    created get_add() function to get questions and answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,150 +1,170 @@
-const quizContainer = document.getElementById('quizContainerNameHere');
+import startFirebase from "./firebase.js";
+
+// function will return data exactly like quiz.json.
+// The function itself is async / a promise so make sure to implement 'await' or '.then()'
+async function get_data() {
+  startFirebase();
+  const db = firebase.firestore();
+
+  let result = { questions: [] };
+
+  const reference = await db.collection("questions-and-answers").get();
+  reference.docs.forEach((doc) => {
+    let data = doc.data();
+    result.questions.push({
+      question: doc.id,
+      answers: data.answers,
+      correctIndex: data.correctIndex,
+      explained: data.explained,
+    });
+  });
+  console.log(result);
+  return result;
+}
+
+get_data();
+
+const quizContainer = document.getElementById("quizContainerNameHere");
 let quizFormElement = document.getElementById("quiz-form");
-const resultsContainer = document.getElementById('resultsContainerNameHere');
+const resultsContainer = document.getElementById("resultsContainerNameHere");
 let questionNum = 0; // a global question number
 let usersTotalScore = 0;
-
 
 /**
  * Gets the quiz object and calls the quiz builder.
  * * The questionNum is compared against the length of the object
- * 
+ *
  * * Calls the ***questionBuilder()*** function with the next question.
-  * * Or calls the ***getResults()*** function.
- * 
+ * * Or calls the ***getResults()*** function.
+ *
  * @param {Integer} questionNum - the global question index
  */
-function getQuestions(questionNum){
-    fetch('/data/quiz.json')
-    .then(response => response.json())
-    .then(data => {
+function getQuestions(questionNum) {
+  fetch("/data/quiz.json")
+    .then((response) => response.json())
+    .then((data) => {
       // get the length of the object
-      let limit = (Object.keys(data.questions).length);
+      let limit = Object.keys(data.questions).length;
       // if question num is still in range - build next question
-      if (questionNum < limit){
-        questionBuilder(data.questions, questionNum)
+      if (questionNum < limit) {
+        questionBuilder(data.questions, questionNum);
+      } else {
+        callResults();
       }
-      else{
-        callResults()
-      } 
     });
 }
 
-/** 
+/**
  * **Builds the current question and possible answers into an output array.**
  * * Output gets inserted into the template
  * * Calls the ***checkUsersAnswers()*** function
- * 
+ *
  * @param {object} question - The questions object
  * @param {integer} questionNumber - Defines current question number
  */
-function questionBuilder(question, questionNumber){
-
+function questionBuilder(question, questionNumber) {
   const currentQuestion = question[questionNumber];
   const possibleAnswers = [];
   const output = [];
-
 
   // Here we setup the answers to the current question
   currentQuestion.answers.forEach((answer, answerNumber) => {
     possibleAnswers.push(
       `<label>
       <input type="radio" name="question${questionNumber}Answers" value="${answerNumber}">
-      ${answerNumber+1} : // to have the displayed numbers 1 more than the array index
+      ${
+        answerNumber + 1
+      } : // to have the displayed numbers 1 more than the array index
       ${answer}
-      </label>`)
+      </label>`
+    );
   });
 
+  console.log(currentQuestion.question, possibleAnswers);
 
-  console.log(currentQuestion.question, possibleAnswers)
-  
   // Setup the output that gets pushed to the template
   output.push(
     `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${possibleAnswers.join('')} </div>
+    <div class="answers"> ${possibleAnswers.join("")} </div>
     <button id="submitAnswer">Submit</button>`
   );
-  output.push(`<h2>Please select an answer and click submit</h2>`)
-  quizFormElement.innerHTML = output.join('');
+  output.push(`<h2>Please select an answer and click submit</h2>`);
+  quizFormElement.innerHTML = output.join("");
 
   // Now we get the submitted answer
   checkUsersAnswer(currentQuestion, questionNumber);
-  
 }
 
 /**
  * **Checks the users answer and compares it to the correct answer**.
- * 
+ *
  * * Calls the ***updateScores()*** function with the score.
  * * Or calls the ***getQuestions*** function to build the next question.
- * 
- * @param {Object} currentQuestion 
- * @param {Integer} questionNumber 
+ *
+ * @param {Object} currentQuestion
+ * @param {Integer} questionNumber
  */
-function checkUsersAnswer(currentQuestion, questionNumber){
-
+function checkUsersAnswer(currentQuestion, questionNumber) {
   const correctAnswer = currentQuestion.correctIndex;
-  
+
   let selectedAnswer;
 
-  document.addEventListener("submit", function(event){
+  document.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let options = document.getElementsByName(`question${questionNumber}Answers`);
-    
+    let options = document.getElementsByName(
+      `question${questionNumber}Answers`
+    );
+
     // loop over answers and see what has been selected
-    if (options){
-      for (let i=0; i<options.length; i++) {
+    if (options) {
+      for (let i = 0; i < options.length; i++) {
         if (options[i].checked) {
           selectedAnswer = options[i].value;
 
           // Check if selection matches correct answer and call correct function
-          if (selectedAnswer == correctAnswer){
-            console.log("Thats a match!")
+          if (selectedAnswer == correctAnswer) {
+            console.log("Thats a match!");
             updateScore(10);
-          }
-          else{
-            console.log("Thats not a match!")
+          } else {
+            console.log("Thats not a match!");
             questionNum += 1;
             getQuestions(questionNum);
           }
         }
       }
     }
-  })
+  });
 }
 
-/** 
+/**
  * **Updates the users score**.
- * 
+ *
  * * Increments questionNum.
  * * Calls the ***getQuestions()*** function for the next question
- * 
- * @param {*} score 
- * @param {*} status 
+ *
+ * @param {*} score
+ * @param {*} status
  */
-function updateScore(score){
+function updateScore(score) {
   usersTotalScore += score;
 
   // call next Question
-  questionNum +=1;
+  questionNum += 1;
   getQuestions(questionNum);
 }
 
 /**
  * Handles displaying the results of the quiz.
  */
-function callResults(){
-  if (usersTotalScore < 30){
+function callResults() {
+  if (usersTotalScore < 30) {
     quizFormElement.innerHTML = `<h1>Terrible! St. Patrick?? You are like an Anti-Patrick - your score is ${usersTotalScore}% GoodBye</h1>`;
-  }
-  else if (usersTotalScore >= 30 && usersTotalScore < 60){
+  } else if (usersTotalScore >= 30 && usersTotalScore < 60) {
     quizFormElement.innerHTML = `<h1>Congratulations!! You compare to St. Patrick in the same way I compare to Brad Pitt! - your score is ${usersTotalScore}%</h1>`;
-  }
-  else {
+  } else {
     quizFormElement.innerHTML = `<h1>Heeelllo Saint Patrick! Alive and well you are looking!! Your score is ${usersTotalScore}%</h1>`;
   }
-  
 }
 
 getQuestions(questionNum);

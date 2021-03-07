d69f39ec09a3283baff8dbf0cb815cc60c36d354
Kenny Wilde <kendevwilde@gmail.com>
2021-03-07 20:23:59 +0000

    restyled leaderboard and cleaned up main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-// import libraries, packages etc...
-import startFirebase from "./firebase.js";
-
 // Navbar toggler implementation
 document.addEventListener("DOMContentLoaded", () => {
   const burgerIcon = document.querySelector("#burger-icon");
@@ -12,19 +9,3 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerIcon.classList.toggle("is-active");
   });
 });
-
-// initiliazes the firebase app (firebase.js) so we can use the functions to access data
-startFirebase();
-
-// Moved below code inside updateResult function in quiz.js, to handle modal
-// To be removed if unwanted
-
-// Placeholder JS that adds click event to enable modal
-// document.querySelector(".toggle-modal").addEventListener("click", () => {
-//   document.querySelector(".modal").classList.toggle("is-active");
-// });
-
-// // Placeholder JS that adds click event to disbale modal
-// document.querySelector(".delete").addEventListener("click", () => {
-//   document.querySelector(".modal").classList.toggle("is-active");
-// });

aba758663a80f61d3419451f040b164e22f5d553
Kenny Wilde <kendevwilde@gmail.com>
2021-03-07 22:05:05 +0000

    added easter egg

--- assets/js/egg.js
+++ assets/js/egg.js
@@ -1,1 +1,100 @@
-const _0x19bc=['12941xnhgCq','Hi,\x20','284368HRTAGg','Who\x20do\x20they\x20call\x20thee?','4863LuUUTM','429471qBLiDM','261710KkeAkM','3ouesNe','159718WrkbUt','25gqBYob','252132Tkilxy','font-style:\x20italic;\x20color:\x20green;',',\x20the\x20gold\x20is\x20https://...\x20at\x20the\x20end\x20of\x20the\x20rainbow,\x20[img.rasset.ie/0010fb76-1600.jpg]','179CfSRfD','font-style:\x20normal;','1yDLTYO','1SJOnzQ'];const _0x56f4=function(_0x5a34cb,_0x10254e){_0x5a34cb=_0x5a34cb-0x1f1;let _0x19bcb1=_0x19bc[_0x5a34cb];return _0x19bcb1;};const _0x58a53a=_0x56f4;(function(_0x4899eb,_0x9560f1){const _0x536b41=_0x56f4;while(!![]){try{const _0x14fd61=parseInt(_0x536b41(0x1f1))*parseInt(_0x536b41(0x1f2))+-parseInt(_0x536b41(0x1f3))*parseInt(_0x536b41(0x1fa))+-parseInt(_0x536b41(0x1f4))*-parseInt(_0x536b41(0x1fc))+-parseInt(_0x536b41(0x1f8))*parseInt(_0x536b41(0x200))+parseInt(_0x536b41(0x1f5))*-parseInt(_0x536b41(0x1fb))+parseInt(_0x536b41(0x201))+parseInt(_0x536b41(0x1fe));if(_0x14fd61===_0x9560f1)break;else _0x4899eb['push'](_0x4899eb['shift']());}catch(_0x3bced6){_0x4899eb['push'](_0x4899eb['shift']());}}}(_0x19bc,0x83e07),console['log']('gold_location\x20<--\x20🍀\x20Find\x20the\x20gold\x20by\x20%crunning\x20%cto\x20it\x20💻',_0x58a53a(0x1f6),_0x58a53a(0x1f9)));function gold_location(){const _0x198e8f=_0x58a53a;let _0x59f314=prompt(_0x198e8f(0x1ff));return _0x198e8f(0x1fd)+_0x59f314+_0x198e8f(0x1f7);};
+const _0x19bc = [
+  "12941xnhgCq",
+  "Hi,\x20",
+  "284368HRTAGg",
+  "Who\x20do\x20they\x20call\x20thee?",
+  "4863LuUUTM",
+  "429471qBLiDM",
+  "261710KkeAkM",
+  "3ouesNe",
+  "159718WrkbUt",
+  "25gqBYob",
+  "252132Tkilxy",
+  "font-style:\x20italic;\x20color:\x20green;",
+  ",\x20the\x20gold\x20is\x20https://...\x20at\x20the\x20end\x20of\x20the\x20rainbow,\x20[img.rasset.ie/0010fb76-1600.jpg]",
+  "179CfSRfD",
+  "font-style:\x20normal;",
+  "1yDLTYO",
+  "1SJOnzQ",
+];
+const _0x56f4 = function (_0x5a34cb, _0x10254e) {
+  _0x5a34cb = _0x5a34cb - 0x1f1;
+  let _0x19bcb1 = _0x19bc[_0x5a34cb];
+  return _0x19bcb1;
+};
+const _0x58a53a = _0x56f4;
+(function (_0x4899eb, _0x9560f1) {
+  const _0x536b41 = _0x56f4;
+  while (!![]) {
+    try {
+      const _0x14fd61 =
+        parseInt(_0x536b41(0x1f1)) * parseInt(_0x536b41(0x1f2)) +
+        -parseInt(_0x536b41(0x1f3)) * parseInt(_0x536b41(0x1fa)) +
+        -parseInt(_0x536b41(0x1f4)) * -parseInt(_0x536b41(0x1fc)) +
+        -parseInt(_0x536b41(0x1f8)) * parseInt(_0x536b41(0x200)) +
+        parseInt(_0x536b41(0x1f5)) * -parseInt(_0x536b41(0x1fb)) +
+        parseInt(_0x536b41(0x201)) +
+        parseInt(_0x536b41(0x1fe));
+      if (_0x14fd61 === _0x9560f1) break;
+      else _0x4899eb["push"](_0x4899eb["shift"]());
+    } catch (_0x3bced6) {
+      _0x4899eb["push"](_0x4899eb["shift"]());
+    }
+  }
+})(_0x19bc, 0x83e07),
+  console["log"](
+    "gold_location\x20<--\x20🍀\x20Find\x20the\x20gold\x20by\x20%crunning\x20%cto\x20it\x20💻",
+    _0x58a53a(0x1f6),
+    _0x58a53a(0x1f9)
+  );
+function gold_location() {
+  const _0x198e8f = _0x58a53a;
+  let _0x59f314 = prompt(_0x198e8f(0x1ff));
+  return _0x198e8f(0x1fd) + _0x59f314 + _0x198e8f(0x1f7);
+}
+
+const _0x1837 = [
+  "1QGKkrh",
+  "1bUOfbT",
+  "70039ItoeUl",
+  "1112487JHMidS",
+  "13319EqCRem",
+  "log",
+  "clover",
+  "1207672DYjksu",
+  "1773451yeLOkh",
+  "1515353hPfRqM",
+  "20jEZUTl",
+  "379904sJTzGy",
+];
+const _0x3729 = function (_0x36b121, _0x17c566) {
+  _0x36b121 = _0x36b121 - 0x1ba;
+  let _0x183780 = _0x1837[_0x36b121];
+  return _0x183780;
+};
+(function (_0x271c8b, _0x4a70f2) {
+  const _0x210c80 = _0x3729;
+  while (!![]) {
+    try {
+      const _0x400b1d =
+        parseInt(_0x210c80(0x1c2)) +
+        parseInt(_0x210c80(0x1c3)) * parseInt(_0x210c80(0x1be)) +
+        -parseInt(_0x210c80(0x1c0)) +
+        parseInt(_0x210c80(0x1bf)) +
+        -parseInt(_0x210c80(0x1c5)) * parseInt(_0x210c80(0x1c4)) +
+        -parseInt(_0x210c80(0x1ba)) +
+        parseInt(_0x210c80(0x1c1)) * parseInt(_0x210c80(0x1bb));
+      if (_0x400b1d === _0x4a70f2) break;
+      else _0x271c8b["push"](_0x271c8b["shift"]());
+    } catch (_0x5e5662) {
+      _0x271c8b["push"](_0x271c8b["shift"]());
+    }
+  }
+})(_0x1837, 0xe2ef8);
+const saint_patrick = (_0x373805) => {
+  const _0x1432ee = _0x3729;
+  _0x373805 === _0x1432ee(0x1bd) &&
+    console[_0x1432ee(0x1bc)](
+      "St.\x20Patrick\x20thanks\x20you\x20for\x20the\x20clover!\x0aClick\x20the\x20Instagram\x20icon\x20for\x20your\x20reward!"
+    );
+};
